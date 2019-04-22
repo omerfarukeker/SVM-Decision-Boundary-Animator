316036bd477240dcd051566ca4a2cd934c382e69
Omer Faruk Eker <43220996+omerfarukeker@users.noreply.github.com>
2019-04-22 20:40:36 +0100

    margins are added

--- svm_boundary_line_animation.py
+++ svm_boundary_line_animation.py
@@ -32,13 +32,16 @@ df = df.reindex(np.random.permutation(df.index))
 df = df.reset_index(drop=True)
 
 #%% train SVM dynamically by adding new data in the loop
-from sklearn import linear_model
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from celluloid import Camera
 from svm_boundary_line_drawer import svm_boundary_line
 
 #generate an SVM model
-SVM_model = LinearSVC()
+SVM_model = SVC(kernel="linear")
+
+##generate an SVM model (alternative)
+#from sklearn.svm import LinearSVC
+#SVM_model = LinearSVC()
 
 #train the SVM model with the first 10 instances
 #otherwise SVM will throw error when tried to train SVM with single class data
@@ -69,19 +72,22 @@ for i in range(init_rows+1,len(df_targets)):
     w1 = SVM_model.coef_[0][0]
     w2 = SVM_model.coef_[0][1]
     
+    s_vectors = SVM_model.support_vectors_[0]
     
     #plot the data and model
     l = df["petal length (cm)"][:i]
     w = df["petal width (cm)"][:i]
     t = df["Targets"][:i]
+#    f1 = ax.scatter(l[t==classes[0]],w[t==classes[0]],c="cornflowerblue",marker="o",s=100,edgecolors="steelblue")
+#    f2 = ax.scatter(l[t==classes[1]],w[t==classes[1]],c="sandybrown",marker="^",s=100,edgecolors="sienna")
     f1 = ax.scatter(l[t==classes[0]],w[t==classes[0]],c="cornflowerblue",marker="o")
     f2 = ax.scatter(l[t==classes[1]],w[t==classes[1]],c="sandybrown",marker="^")
     scores.append(SVM_model.score(X,y))
     
     #draw the SVM boundary line
-    func_text_pos_y = svm_boundary_line(ax,w1,w2,w0)
+    func_text_pos_y = svm_boundary_line(ax,w1,w2,w0,s_vectors)
     
-    #title wont work with celluloid package, text is an alternative to workaround 
+    #title wont work with celluloid package, text is an alternative to workaround
     ax.text(0.25, 1.03, "SVM Training Accuracy: %.2f"%(SVM_model.score(X,y)), 
             fontweight="bold", transform=ax.transAxes)
     ax.set_xlim([-3,3])
@@ -92,8 +98,11 @@ for i in range(init_rows+1,len(df_targets)):
 #            fontweight="bold", transform=ax.transAxes)
     
     #dynamic text position for the boundary function
-    ax.text(-2, func_text_pos_y, r"$y = \frac{%.2f}{%.2f}+\frac{%.2f}{%.2f}x$"%(-w0,w2,-w1,w2), 
-            fontweight="bold")
+    if func_text_pos_y < 2.95:
+        ax.text(-2, func_text_pos_y, r"$y = \frac{%.2f}{%.2f}+\frac{%.2f}{%.2f}x$"%(-w0,w2,-w1,w2), 
+                fontweight="bold")
+    
+        
     ax.legend([f1,f2],iris.target_names[flowers])
    
     #take a snapshot of the figure
--- svm_boundary_line_drawer.py
+++ svm_boundary_line_drawer.py
@@ -5,9 +5,24 @@ SVM boundary line drawing function
 @author: omerzulal
 """
 import numpy as np
-def svm_boundary_line(ax,w1,w2,w0):
+
+def svm_boundary_line(ax,w1,w2,w0,s_vectors):
     """Plot a line from slope and intercept"""
     x_vals = np.array(ax.get_xlim())
     y_vals = -w0/w2 + (-w1/w2) * x_vals
-    ax.plot(x_vals, y_vals, ':k')
+    
+    margin = 1 / np.sqrt(np.sum(np.array([w1,w2]) ** 2))
+    margin_bot = y_vals - np.sqrt(1 + (-w1/w2) ** 2) * margin
+    margin_top = y_vals + np.sqrt(1 + (-w1/w2) ** 2) * margin
+
+    #plot the hyperplane
+    ax.plot(x_vals, y_vals, '--',c="dimgrey")
+    
+    #plot the margins
+    ax.plot(x_vals, margin_top,':',c="darkgrey")
+    ax.plot(x_vals, margin_bot,':',c="darkgrey")
+    
     return -w0/w2 + (-w1/w2) * -2
+
+
+
